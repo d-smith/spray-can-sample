8dff81a4f6200fa92171ebc0beeafbd0e3785729
d-smith <doug.smith.mail@gmail.com>
2014-01-14 10:05:00 -0700

    added a simple spray routing example

---
+++ src/main/scala/sample/SomeRoutes.scala
@@ -0,0 +1,70 @@
+package sample
+
+import spray.routing._
+import akka.io.IO
+import spray.can.Http
+import akka.actor.{Actor, Props, ActorSystem}
+
+class PingDirectives extends Directives {
+  def route = path("ping") {
+    get {
+      ctx => {
+        ctx.complete("PING!!!")
+      }
+    }
+  }
+}
+
+class PongDirectives extends Directives {
+  def route = path("pong") {
+    get {
+      ctx => {
+        ctx.complete("PONG!!!")
+      }
+    }
+  }
+}
+
+class GnopDirectives extends Directives {
+  def route = path("gnop") {
+    get {
+      ctx => {
+        ctx.complete("GNOP!!!")
+      }
+    }
+  }
+}
+
+class RoutedHttpService(route: Route) extends Actor with HttpService {
+  implicit def actorRefFactory = context
+  def receive = runRoute(route)
+}
+
+object RouteConconcatenator extends RouteConcatenation {
+  def concatRoutes(chain: Route, routes: List[Route]) : Route = {
+    routes match {
+      case Nil => chain
+      case h :: Nil => chain ~ h
+      case h :: t => concatRoutes(chain ~ h, t)
+    }
+  }
+}
+
+// This main simulates the case where we might find all the directives in an OSGi container
+// that have been exported as services, and build a concatenated route to allow a single
+// HTTP listener route traffic to the routes in the container.
+object Main2 extends App with RouteConcatenation {
+  import RouteConconcatenator.concatRoutes
+
+  // We can get a list of exported directives in an OSGi container...
+  val routes = List(new PingDirectives().route,
+    new PongDirectives().route,
+    new GnopDirectives().route)
+
+  // Transform a list of routes into a concatenated route
+  val chainedRoutes = concatRoutes(routes.head, routes.tail)
+
+  implicit val system = ActorSystem()
+  val rootService = system.actorOf(Props(new RoutedHttpService(chainedRoutes)))
+  IO(Http)(system) ! Http.Bind(rootService, "0.0.0.0", port = 8666)
+}
